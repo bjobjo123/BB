9ac8004f27c69699ac78e7a4a3984e2ed59f6785
bjobjo123 <bjobjo.eb@gmail.com>
2025-01-17 11:34:13 +0100

    tweak to server

--- script.mjs
+++ script.mjs
@@ -11,6 +11,10 @@ function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
+app.get('/tmp/poem', function (req, res) {
+    res.send("Hører du fjellene Det er fjellene som står der Hører du dem? Det er stillheten du hører.")
+  })
+
 server.get("/", getRoot);
 
 server.listen(server.get('port'), function () {
